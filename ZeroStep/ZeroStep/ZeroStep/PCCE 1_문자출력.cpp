dd93028787ee763b714398d0300ecf45fd2b7865
Min-seo-Choi <mschoi0814@gmail.com>
2025-03-18 12:42:28 +0900

    PCCE 기출문제 문자출력, 각도 합치기, 수 나누기, 병과 분류, 심폐소생술 완료

---
+++ ZeroStep/ZeroStep/ZeroStep/PCCE 1_문자출력.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// 주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+int main(void) {
+	string message = "Let's go!";
+
+	cout << "3 \n 2 \n 1" << endl;
+	cout << message << endl;
+
+	return 0;
+}
